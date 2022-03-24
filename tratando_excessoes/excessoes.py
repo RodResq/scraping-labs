4c4c4912329c04c7615a6f7b26176bdf65c65e59
RodResq <rodrigoresque@yahoo.com.br>
2022-03-24 00:43:13 -0300

    add gitignore

--- tratando_excessoes/excessoes.py
+++ tratando_excessoes/excessoes.py
@@ -1,6 +1,13 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+from urllib.error import HTTPError, URLError
 
-html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
-bs = BeautifulSoup(html.read(), "html.parser")
-print(bs.h1)
+try:
+    html = urlopen("http://www.pythonscraping.com/exercises/exercise1.html")
+except HTTPError as e:
+    print(e)
+except URLError:
+    print('The server could not be found ...')
+else:
+    bs = BeautifulSoup(html, "html.parser")
+    print(bs.html.body.h1)
